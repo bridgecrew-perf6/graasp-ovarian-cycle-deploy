8c9aeb879b120b4779958286a62cfcefc0971261
scanf6 <scanf555@gmail.com>
2020-12-09 13:43:48 +0300

    Initial commit

---
+++ precache-manifest.240e5fe50bbdc7fe0d6ee9ee5550fc97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "38ba084d54036b581701",
+    "url": "./static/css/main.9d7d395d.chunk.css"
+  },
+  {
+    "revision": "38ba084d54036b581701",
+    "url": "./static/js/main.38ba084d.chunk.js"
+  },
+  {
+    "revision": "bfb2c1b477b6d5590f22",
+    "url": "./static/css/1.41c5d8a7.chunk.css"
+  },
+  {
+    "revision": "bfb2c1b477b6d5590f22",
+    "url": "./static/js/1.bfb2c1b4.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "db53f63117c8f72b01c414a9b3129310",
+    "url": "./static/media/egg_cell.db53f631.png"
+  },
+  {
+    "revision": "613b5ab759d2f8d1d37e30a527b8bec9",
+    "url": "./static/media/GraaspLogo.613b5ab7.svg"
+  },
+  {
+    "revision": "cf6c283e3c75a9bcbd1d4ecd9ce686b8",
+    "url": "./index.html"
+  }
+];
